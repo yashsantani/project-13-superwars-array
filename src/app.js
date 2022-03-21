39457d502887560575711702805b64b031984c97
yashsantani <yashsantani60@gmail.com>
2022-03-21 12:59:42 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,59 +1,78 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+  let detailedPlayers = [];
 
-    // Instead of forloop use Map method
-    // Code here
-
-    return detailedPlayers;
-}
+  // Instead of forloop use Map method
+  // Code here
+  players.map((player, i) => {
+    let char = {};
+    char.name = player;
+    char.strength = getRandomStrength();
+    char.image = `images/super-${i + 1}.png`;
+    char.type = getRandomStrength() % 2 == 0 ? "hero" : "villain";
+    detailedPlayers.push(char);
+  });
+  return detailedPlayers;
+};
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
+  return Math.ceil(Math.random() * 100);
+};
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+  let fragment = "";
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+  // Instead of using for loop
+  // Use chaining of Array methods - filter, map and join
+  // Type your code here
+  fragment = players
+    .filter((player) => player.type.toLowerCase() == type.toLowerCase())
+    .map((player, i) => {
+      return `<div class="player">
+                <img src="${player.image}" alt="">
+                <div class="name">${player.name}</div>
+                <div class="strength">${player.strength}</div>
+            </div>`;
+    })
+    .join("");
 
-    return fragment;
-}
+  return fragment;
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+  document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML = buildPlayers(
+    players,
+    "villain"
+  );
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
